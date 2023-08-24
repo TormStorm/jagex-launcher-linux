2e705d72323972b4b336b52f5eac1f2fc68d17a9
tryorerror <32785184+tryorerror@users.noreply.github.com>
2023-08-24 11:07:16 +0400

    Update runescape.sh
    Added options where runescape will use nvidia gpu as the main renderer

--- resources/runescape.sh
+++ resources/runescape.sh
@@ -3,6 +3,8 @@ set -e
 flatpak run --env=PULSE_LATENCY_MSEC=200 \
     --env='vblank_mode=0' \
     --env='MESA_GL_VERSION_OVERRIDE=4.5FC' \
+    --env='__NV_PRIME_RENDER_OFFLOAD=1' \
+    --env='__GLX_VENDOR_LIBRARY_NAME=nvidia' \
     com.jagex.RuneScape || \
 flatpak-spawn --host flatpak run \
     --env=PULSE_LATENCY_MSEC=200 \
@@ -13,4 +15,6 @@ flatpak-spawn --host flatpak run \
     --env=JX_DISPLAY_NAME="$JX_DISPLAY_NAME" \
     --env=JX_REFRESH_TOKEN="$JX_REFRESH_TOKEN" \
     --env=JX_SESSION_ID="$JX_SESSION_ID" \
+    --env='__NV_PRIME_RENDER_OFFLOAD=1' \
+    --env='__GLX_VENDOR_LIBRARY_NAME=nvidia' \
     com.jagex.RuneScape
