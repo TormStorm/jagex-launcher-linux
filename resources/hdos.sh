6c11729f278d0a8a042562bf30696cfe98f5c83c
Torm <35997481+TormStorm@users.noreply.github.com>
2023-09-08 09:26:54 +0000

    Update hdos.sh

--- resources/hdos.sh
+++ resources/hdos.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-flatpak run dev.hdos.HDOS
+flatpak-spawn --host flatpak run \
+    --env=PULSE_LATENCY_MSEC=200 \
+    --env='vblank_mode=0' \
+    --env='MESA_GL_VERSION_OVERRIDE=4.5FC' \
+    --env=JX_ACCESS_TOKEN="$JX_ACCESS_TOKEN" \
+    --env=JX_CHARACTER_ID="$JX_CHARACTER_ID" \
+    --env=JX_DISPLAY_NAME="$JX_DISPLAY_NAME" \
+    --env=JX_REFRESH_TOKEN="$JX_REFRESH_TOKEN" \
+    --env=JX_SESSION_ID="$JX_SESSION_ID" \
+    dev.hdos.HDOS
