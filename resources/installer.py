d7dbaea9ba4e019ecd4321939c83c1f5c4526970
Jacob McSwain <jagex-launcher-flatpak@mcswain.dev>
2023-12-08 16:50:35 -0600

    installer: test using jagex.akamaized.net domain for SSL validation

--- resources/installer.py
+++ resources/installer.py
@@ -2,21 +2,13 @@ import requests
 import json
 import base64
 import gzip
-import pprint
 import os
 from pathlib import Path
 import errno
-import sys
 import base64
-import jwcrypto.jwt, jwcrypto.jwk
-import cryptography.x509
-from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
-from cryptography.hazmat.primitives.asymmetric import padding
-import pprint
 from hashlib import sha256
-import datetime
 
-url_template = "http://jagex-akamai.aws.snxd.com/direct6/launcher-win/pieces/{}/{}.solidpiece"
+url_template = "https://jagex.akamaized.net/direct6/launcher-win/pieces/{}/{}.solidpiece"
 dirname = os.getcwd()
 
 def mkdir_p(path):
@@ -91,11 +83,7 @@ def cleanup():
         p.unlink()
 
 def main():
-    # TODO make this grab the latest release instead of hard-coding, although this should work for awhile since the launcher auto-updates itself on launch.
-    # That piece reliably on linux.
-    metafile_url_template = "http://jagex-akamai.aws.snxd.com/direct6/launcher-win/metafile/{}/metafile.json"
-    # This is the fingerprint of the certificate that signed the JWT we are using from the jagex CDN so we can validate we are trusting the right certificate chain.
-    JAGEX_PACKAGE_CERTIFICATE_SHA256_HASH = "848bae7e92dc58570db50cdfc933a78204c1b00f05d64f753a307ebbaed2404f"
+    metafile_url_template = "https://jagex.akamaized.net/direct6/launcher-win/metafile/{}/metafile.json"
 
     catalog_url = "https://jagex.akamaized.net/direct6/launcher-win/alias.json"
 
@@ -105,70 +93,16 @@ def main():
 
     print('Latest metafile_url: {}'.format(metafile_url))
 
-    metafile_json = requests.get(metafile_url)
-
-    # Load and deserialize JWT
-    jwt = metafile_json.content.strip()
-    jwt = jwcrypto.jwt.JWT(jwt=jwt.decode("ascii"))
-
-    # Deserialize the leaf certificate and validate the fingerprint of the certificate
-    trust_path = jwt.token.jose_header.get("x5c", [])
-    leaf_cert_b64 = trust_path[0]
-    leaf_cert_sha256_hash = sha256(leaf_cert_b64.encode('utf8')).hexdigest()
-
-    print ("Validating fingerprint of the certificate that signed the JWT...")
-    if leaf_cert_sha256_hash != JAGEX_PACKAGE_CERTIFICATE_SHA256_HASH:
-        raise Exception("The certificate in the JWT header does not match the expected fingerprint.")
-
-    leaf_cert = cryptography.x509.load_der_x509_certificate(
-        base64.b64decode(leaf_cert_b64))
-
-    # Derive public key from the package cert and convert to JWK
-    public_key = leaf_cert.public_key()
-    public_key = public_key.public_bytes(Encoding.PEM, PublicFormat.PKCS1)
-    public_key = jwcrypto.jwk.JWK.from_pem(public_key)
-
-    # Validate JWT and access claims
-    jwt.validate(public_key)
-    print('''The jwt has validated against the certificate. 
-        Issuer: {}
-        Subject: {}
-        Expiration UTC: {}
-        '''.format(leaf_cert.issuer, leaf_cert.subject, leaf_cert.not_valid_after))
-
-    # Build certificate chain
-    trust_path = jwt.token.jose_header.get("x5c", [])
-    trust_path = [
-        cryptography.x509.load_der_x509_certificate(base64.b64decode(cert))
-        for cert in trust_path
-    ]
-
-    # Verify certificate chain
-    for i in range(len(trust_path) - 1):
-        issuer_certificate = trust_path[i + 1]
-        subject_certificate = trust_path[i]
-        issuer_public_key = issuer_certificate.public_key()
-        issuer_public_key.verify(
-            subject_certificate.signature,
-            subject_certificate.tbs_certificate_bytes,
-            padding.PKCS1v15(),
-            subject_certificate.signature_hash_algorithm,
-        )
-        # Verify certificate expiration
-        current_time = datetime.datetime.utcnow()
-        if current_time < issuer_certificate.not_valid_before or current_time > issuer_certificate.not_valid_after:
-            raise Exception("Issuer certificate has expired.")
+    metafile_json = json.loads(requests.get(metafile_url).content)
 
     try:
         global digest_list
         global pad_array
         global file_list
-        
-        verified_claims_json = json.loads(jwt.claims)
 
-        digest_list = verified_claims_json.get("pieces").get("digests")
-        file_list = verified_claims_json.get("files")
-        pad_array = verified_claims_json.get("pad")
+        digest_list = metafile_json.get("pieces").get("digests")
+        file_list = metafile_json.get("files")
+        pad_array = metafile_json.get("pad")
 
         downloaded_file_pieces = []
 
